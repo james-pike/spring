5a7aa6f62a514e8c6dbe5228b615fce37f51ad1b
Tester <test@email.com>
2025-03-18 15:25:53 -0400

    commit

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -12,7 +12,7 @@ export default component$(() => {
         <div class="py-12 md:py-20 lg:py-0 lg:flex lg:items-center lg:h-screen lg:gap-8">
           <div class="basis-1/2 text-center lg:text-left pb-10 md:pb-16 mx-auto">
             <h1 class="text-5xl md:text-6xl font-bold leading-tighter tracking-tighter mb-4 font-heading dark:text-gray-200">
-            Trusted Locksmith Services & Security Solutions <br class="hidden lg:block" />{" "}
+            Trusted Locksmith & Security Solutions <br class="hidden lg:block" />{" "}
               {/* <span class="hidden lg:inline">create a website using </span> <span class="text-[#039de1]">Qwik</span> +{" "}
               <span class="sm:whitespace-nowrap text-[#039de1]">Tailwind CSS</span> */}
             </h1>
