5209ca6f24c3ae6ad9b145f1e2b690fb859a0d02
Tester <test@email.com>
2025-03-18 15:58:27 -0400

    commit

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -9,15 +9,15 @@ export default component$(() => {
       <div class="absolute inset-0 pointer-events-none" aria-hidden="true"></div>
       <div class="relative max-w-7xl mx-auto px-4 sm:px-6">
         <div class="pt-0 md:pt-[76px] pointer-events-none"></div>
-        <div class="py-10 md:py-20 lg:py-0 lg:flex lg:items-center lg:h-screen lg:gap-8">
-          <div class="basis-1/2 text-center lg:text-left pb-10 md:pb-16 mx-auto">
+        <div class="py-8 md:py-20 lg:py-0 lg:flex lg:items-center lg:h-screen lg:gap-8">
+          <div class="basis-1/2 text-center lg:text-left pb-8 md:pb-16 mx-auto">
             <h1 class="text-5xl md:text-6xl font-bold leading-tighter tracking-tighter mb-4 font-heading dark:text-gray-200">
             Trusted Locksmith & Security Solutions <br class="hidden lg:block" />{" "}
               {/* <span class="hidden lg:inline">create a website using </span> <span class="text-[#039de1]">Qwik</span> +{" "}
               <span class="sm:whitespace-nowrap text-[#039de1]">Tailwind CSS</span> */}
             </h1>
             <div class="max-w-3xl mx-auto lg:max-w-none">
-              <p class="text-xl text-muted mb-6 dark:text-slate-300">
+              <p class="text-xl text-muted mb-5 dark:text-slate-300">
             
                 AdeptLock provides expert locksmith services for homes, businesses, and vehicles. 
                 From emergency lockouts to advanced security installations, we ensure your safety with precision and care.
@@ -25,7 +25,7 @@ export default component$(() => {
            
               </p>
 
-              <div class="max-w-xs sm:max-w-md m-auto flex flex-nowrap flex-col sm:flex-row sm:justify-center gap-4 lg:justify-start lg:m-0 lg:max-w-7xl">
+              <div class="max-w-xs sm:max-w-md m-auto flex flex-nowrap flex-col sm:flex-row sm:justify-center gap-3 lg:justify-start lg:m-0 lg:max-w-7xl">
                 <div class="flex w-full sm:w-auto">
                   <a
                     class="btn btn-primary sm:mb-0 w-full"
