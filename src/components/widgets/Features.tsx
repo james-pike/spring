b510dc18b1f0152e9f26a395901d3bd2775c0a29
Tester <test@email.com>
2025-03-19 15:06:57 -0400

    commit

--- src/components/widgets/Features.tsx
+++ src/components/widgets/Features.tsx
@@ -2,7 +2,6 @@ import { component$ } from "@builder.io/qwik";
 import { twMerge } from "tailwind-merge";
 import { Headline } from "~/components/ui/Headline";
 import { ItemGrid } from "~/components/ui/ItemGrid";
-import FAQAccordion from "./FAQAccordion";
 
 interface Item {
   title?: string;
