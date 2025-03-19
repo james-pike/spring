33bd09e4ea8d9482a9ed263695b674ad799990a5
Tester <test@email.com>
2025-03-19 15:05:14 -0400

    commit

--- src/components/widgets/Features.tsx
+++ src/components/widgets/Features.tsx
@@ -33,38 +33,33 @@ export default component$((props: Props) => {
   } = props;
 
   return (
-    <section class="relative" {...(id ? { id } : {})}>
-      <div class="absolute inset-0 pointer-events-none -z-[1]" aria-hidden="true">
-        <slot name="bg">
-          <div class={twMerge("absolute inset-0", isDark ? "bg-dark dark:bg-transparent" : "")}></div>
-        </slot>
-      </div>
-      <div
-        class={twMerge("relative text-default px-4 md:px-6 py-12 md:py-16 lg:py-20 mx-auto max-w-6xl", isDark ? "dark" : "")}
-      >
-        <Headline
-          title={title}
-          subtitle={subtitle}
-          highlight={highlight}
-          classes={{
-            container: "max-w-xl sm:mx-auto lg:max-w-2xl",
-            title: "sm:text-4xl text-3xl",
-            ...(classes?.headline ?? {}),
-          }}
-        />
-        <div class="sm:mx-auto hidden sm:block">
+    <section class="relative scroll-mt-16" {...(id ? { id } : {})}>
+    <div class="absolute inset-0 pointer-events-none -z-[1]" aria-hidden="true">
+      <slot name="bg">
+        <div class={twMerge("absolute inset-0", isDark ? "bg-dark dark:bg-transparent" : "")}></div>
+      </slot>
+    </div>
+    <div
+      class={twMerge(
+        "relative mx-auto max-w-5xl px-4 md:px-6 py-12 md:py-16 lg:py-20 text-default",
+        classes?.container,
+        isDark ? "dark" : ""
+      )}
+    >
+      <Headline title={title} subtitle={subtitle} highlight={highlight} classes={classes?.headline} />
+        <div class="sm:mx-auto">
           <ItemGrid
             id="features-grid" // Unique ID for this instance
             items={items}
             classes={{
-              panel: "max-w-none",
+              container: "md:grid-cols-2",
+              title: "md:text-[1.3rem]",
+              icon: "text-white bg-secondary-500 dark:bg-secondary-700 rounded-full w-10 h-10 p-2 md:w-12 md:h-12 md:p-3 mr-4",
               ...(classes?.items ?? {}),
             }}
           />
         </div>
-        <div class="block sm:hidden">
-          <FAQAccordion />
-        </div>
+      
       </div>
     </section>
   );
