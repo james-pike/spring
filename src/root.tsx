282224db9e4e171285718a91d0b2a0e9e3f839f8
Tester <test@email.com>
2025-03-22 17:11:56 -0400

    commit

--- src/root.tsx
+++ src/root.tsx
@@ -32,6 +32,19 @@ export default component$(() => {
         <RouterHead />
         <DarkThemeLauncher />
         <ServiceWorkerRegister />
+                <script
+          dangerouslySetInnerHTML={`
+            window.$crisp = [];
+            window.CRISP_WEBSITE_ID = "283b7c7f-4f69-4725-ba15-f11822e24856";
+            (function() {
+              var d = document;
+              var s = d.createElement("script");
+              s.src = "https://client.crisp.chat/l.js";
+              s.async = 1;
+              d.getElementsByTagName("head")[0].appendChild(s);
+            })();
+          `}
+        />
         <link rel="preload" href="/images/placeholder.png" as="image" />
       </head>
       <body class="text-gray-900 dark:text-slate-300 tracking-tight md:border-x border-gray-700 bg-white max-w-7xl mx-auto  dark:bg-gray-950 antialiased">
