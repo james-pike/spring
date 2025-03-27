ea558bf0e504c6754a712e254f786c7b3ca6a7b6
Tester <test@email.com>
2025-03-27 19:34:29 -0400

    Integrate Theme Customization

--- src/components/widgets/Pricing.tsx
+++ src/components/widgets/Pricing.tsx
@@ -28,7 +28,7 @@ export default component$((props: Props) => {
     title = null,
     subtitle = null,
     highlight = null,
-    items = [],
+
     isDark = false,
     classes = {},
   } = props;
