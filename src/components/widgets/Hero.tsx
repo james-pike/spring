845b201086e32d4b070ad1e1bcc0a579ed82a775
Tester <test@email.com>
2025-04-04 15:42:45 -0400

    commit

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -14,7 +14,7 @@ export default component$(() => {
         {/* Text Content */}
         <div class="relative z-10  order-1 md:order-1 flex items-center justify-center  px-4 py-8 md:px-8 md:py-0">
           <div class="text-center md:text-left">
-            {/* 
+            {/* /
           <a href="#" class="inline-flex justify-between items-center py-1 -mt-1 px-1 pr-4 mb-4 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
             <span class="text-xs bg-primary rounded-full text-white px-4 py-1.5 mr-3">New</span> <span class="text-sm font-medium">Free Web Audit & SEO Report</span> 
             <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
