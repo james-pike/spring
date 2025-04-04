8819bcdc2f639d03d4b8b33819bfd26954d862e8
Tester <test@email.com>
2025-04-03 20:18:48 -0400

    commit

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -4,18 +4,20 @@ import LogoClouds from "./LogoClouds";
 import { Button } from "../ui/Button";
 import { Link } from "@builder.io/qwik-city";
 
+// bg-gradient-to-r from-muted to-background dark:from-background dark:to-muted
+
 export default component$(() => {
   return (
     <section class="relative overflow-hidden">
-      <div class="grid grid-cols-1 md:grid-cols-2 grid-rows-[auto_auto] md:grid-rows-[85%_15%] min-h-[calc(100vh-89px)]">
+      <div class="grid stars grid-cols-1 md:grid-cols-2 grid-rows-[auto_auto] md:grid-rows-[85%_15%] min-h-[calc(100vh-89px)]">
         {/* Text Content */}
-        <div class="relative z-10 stars order-1 md:order-1 flex items-center justify-center bg-gradient-to-r from-muted to-background dark:from-background dark:to-muted px-4 py-8 md:px-8 md:py-0">
+        <div class="relative z-10  order-1 md:order-1 flex items-center justify-center  px-4 py-8 md:px-8 md:py-0">
           <div class="text-center md:text-left">
-{/* 
+            {/* 
           <a href="#" class="inline-flex justify-between items-center py-1 -mt-1 px-1 pr-4 mb-4 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
             <span class="text-xs bg-primary rounded-full text-white px-4 py-1.5 mr-3">New</span> <span class="text-sm font-medium">Free Web Audit & SEO Report</span> 
             <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-        </a> */}
+           </a> */}
             <h1 class="text-5xl font-bold tracking-tighter text-balance sm:text-6xl md:text-6xl lg:text-7xl mb-4 animate-hero-text">
               Premium <span class="text-primary">Web Design</span> Solutions
             </h1>
@@ -34,7 +36,10 @@ export default component$(() => {
         </div>
         {/* Carousel */}
         <div class="relative order-2 px-4 md:order-2 h-52 sm:h-80 md:h-full">
-          <Carousel />
+          {/* <Carousel /> */}
+          <object aria-label="SVGator stroke offset animation"  class="cr_jc" type="image/svg+xml" data="https://cdn.svgator.com/assets/main-page/fold5/girl.svg">
+  <img alt="SVGator stroke offset animation" src="https://cdn.svgator.com/assets/main-page/fold5/girl.svg"/>
+</object>
         </div>
         {/* LogoClouds */}
         <div class="bg-gradient-to-r from-gray-200 to-gray-50 dark:from-gray-900 dark:to-gray-700 col-span-1 md:col-span-2 flex items-center justify-center py-4 order-3">
