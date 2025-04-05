20356d62aeebdb5c493f71c708f2299c96e5025a
Tester <test@email.com>
2025-04-05 14:15:47 -0400

    commit

--- src/components/widgets/FAQTabs.tsx
+++ src/components/widgets/FAQTabs.tsx
@@ -1,4 +1,4 @@
-import { component$, useContextProvider } from '@builder.io/qwik';
+import { component$, useContextProvider, useSignal } from '@builder.io/qwik';
 import { Tabs } from '../ui/Tabs';
 import { DarkContext } from '~/DarkContext';
 import FAQAccordion from './FAQAccordion';
@@ -17,6 +17,7 @@ interface Props {
 
 export default component$((props: Props) => {
   const { isDark = false, faqData } = props;
+  const activeTab = useSignal(0); // Track the active tab
 
   useContextProvider(DarkContext, isDark);
 
@@ -24,14 +25,25 @@ export default component$((props: Props) => {
     <div class="flex flex-col md:flex-row gap-12 mx-auto justify-center">
       <Tabs.Root class="max-w-[500px]">
         <Tabs.List class="grid w-full grid-cols-4">
-          {faqData.map((section) => (
-            <Tabs.Tab key={section.title}>{section.title}</Tabs.Tab>
+          {faqData.map((section, index) => (
+            <Tabs.Tab
+              key={section.title}
+              onClick$={() => {
+                activeTab.value = index; // Update active tab on click
+              }}
+            >
+              {section.title}
+            </Tabs.Tab>
           ))}
         </Tabs.List>
 
         {faqData.map((section, index) => (
-          <Tabs.Panel key={index}>
-            <FAQAccordion items={section.items} /> {/* Passing items as a prop */}
+          <Tabs.Panel
+            key={index}
+            class={{ 'hidden': activeTab.value !== index }} // Hide inactive panels
+          >
+            {/* Use activeTab.value in the key to force re-mount on tab switch */}
+            <FAQAccordion key={`faq-${index}-${activeTab.value}`} items={section.items} />
           </Tabs.Panel>
         ))}
       </Tabs.Root>
