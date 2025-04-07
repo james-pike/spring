c1c1981976750e955998ba7953dc80766c47161c
Tester <test@email.com>
2025-04-06 23:32:31 -0400

    commit

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -35,7 +35,7 @@ const Trigger = component$<SelectTriggerProps>((props) => {
     <HeadlessSelect.Trigger
       {...restProps}
       class={cn(
-        'flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border border-input px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
+        'flex h-10 w-full items-center justify-between whitespace-nowrap rounded-base border border-input px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-primary disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
         isDark ? 'bg-muted' : 'bg-popover',
         props.class
       )}
