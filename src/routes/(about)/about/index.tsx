c6a984701bbdc9004d9a5fd89fc912757110d06e
Tester <test@email.com>
2025-04-10 10:28:21 -0400

    commit

--- src/routes/(about)/about/index.tsx
+++ src/routes/(about)/about/index.tsx
@@ -10,41 +10,7 @@ export default component$(() => {
     <>
     
  <About  
-      title="About TestLock"
-      subtitle="About page"
-      highlight="About"
-      items={[
-        {
-          title: "Do you offer emergency locksmith services?",
-          description:
-            "Yes! We provide 24/7 emergency lockout assistance for homes, businesses, and vehicles.",
-        },
-        {
-          title: "Can you replace lost or broken keys?",
-          description:
-            "Absolutely! We offer key duplication, cutting, and replacement services for all types of locks.",
-        },
-        {
-          title: "Do you install smart locks and security systems?",
-          description:
-            "Yes, we specialize in smart lock installation and advanced security systems for added protection.",
-        },
-        {
-          title: "Can you repair or replace damaged locks?",
-          description:
-            "Of course! We repair, rekey, and replace locks to enhance security and restore functionality.",
-        },
-        {
-          title: "Do you provide car key programming and replacement?",
-          description:
-            "Yes! We offer car key programming, key fob replacement, and ignition repair for most vehicle models.",
-        },
-        {
-          title: "Can you open safes and vaults?",
-          description:
-            "Yes, we offer professional safe opening and installation services while ensuring your valuables remain secure.",
-        },
-      ]}
+
     />
      
     </>
