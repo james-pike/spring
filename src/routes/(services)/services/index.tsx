8b35e01131ac0cb3f7f006eb3fb6b7b1258690ee
Tester <test@email.com>
2025-04-11 12:25:00 -0400

    commit

--- src/routes/(services)/services/index.tsx
+++ src/routes/(services)/services/index.tsx
@@ -1,6 +1,5 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import Services from "~/components/widgets/Services";
 import ServicesX from "~/components/widgets/ServicesX";
 
 
