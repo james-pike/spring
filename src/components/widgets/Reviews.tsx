0865b955feb28503e8319ba2a34cd68aba815d58
Tester <test@email.com>
2025-04-10 22:26:11 -0400

    commit

--- src/components/widgets/Reviews.tsx
+++ src/components/widgets/Reviews.tsx
@@ -33,7 +33,7 @@ export default component$((props: Props) => {
   // Conditional padding classes
 
   return (
- <SectionWrapper id={id} isDark={isDark} classes={classes} bgClass=" inset-0 z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]" >
+ <SectionWrapper id={id} isDark={isDark} classes={classes} bgClass=" inset-0 z-10 h-full w-full bg-background bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]" >
      
         <Headline title={title} subtitle={subtitle} highlight={highlight} classes={classes?.headline} />
         <Card.Root class="bg-background">
